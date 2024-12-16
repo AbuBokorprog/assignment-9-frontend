ac9ca449f71c795af105775d7b23abc0e1df910d
AbuBokorprog <abubokor1066@gmail.com>
2024-12-17 00:23:41 +0600

    base api issue fixed.

--- src/redux/features/api/BaseApi.ts
+++ src/redux/features/api/BaseApi.ts
@@ -12,7 +12,7 @@ import { RootState } from '../../store';
 // Define a service using a base URL and expected endpoints
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: `${import.meta.env.BASE_URL}/api`,
+  baseUrl: `https://bazaar-bridge.vercel.app/api`,
   credentials: 'include',
   prepareHeaders: (headers, { getState }) => {
     const token = (getState() as RootState).auth.token;
